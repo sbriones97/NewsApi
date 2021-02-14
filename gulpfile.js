e2fc5aea91885b9d4c4149d7787bcb0cfcf3f84a
sbriones97 <sbriones97@hotmail.com>
2021-02-13 20:07:58 -0600

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+let gulp = require('gulp')
+let sass = require('gulp-sass')
+let typescript = require('gulp-typescript')
+
+gulp.task('html', function() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'))
+})
+gulp.task('styles', function() {
+    return gulp.src('src/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/assets/styles'))
+})
+gulp.task('scripts', function() {
+    let tsConfig = typescript.createProject('tsconfig.json')
+    return tsConfig.src()
+        .pipe(tsConfig())
+        .pipe(gulp.dest('dist/assets/scripts'))
+})
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+// API KEY: 04facfddb30e45f58465cda2b63a9820
+
+
+console.log("HolaMundo")
